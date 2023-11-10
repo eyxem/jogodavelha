eb9d6a69938fa20ad8b53698f3dadb97fccae19e
eyxem <105081099+eyxem@users.noreply.github.com>
2023-11-10 10:52:59 -0300

    sv

--- jogo/jogo.js
+++ jogo/jogo.js
@@ -1,8 +1,14 @@
+//Seleciona todos os elementos com a classe "celula" e os armazena em uma lista.
 const celulas = document.querySelectorAll(".celula");
 
+//Inicializa a variável checarTurno como verdadeira.
 let checarTurno = true;
 
+//Inicializa a variável turno, que será usado para alternar entre "X" e "O".
 let turno;
 
+//Define constantes para representar os jogadores "X" e "O".
 const JOGADOR_X = "X";
 const JOGADOR_O = "O";
+
+//Inicializa a variável jogoAcabou como falso, indicando que
